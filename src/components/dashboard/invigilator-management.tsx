6da5fe33ab828f53ce4dc7914963d3beec5f949c
Lokesh D <lokeshconnects@gmail.com>
2026-01-17 19:00:38 +0000

    Modify font color of the title "Invigilators' Details" to primary blue

--- src/components/dashboard/invigilator-management.tsx
+++ src/components/dashboard/invigilator-management.tsx
@@ -162,7 +162,7 @@ export function InvigilatorManagement() {
         <>
         <Card className="shadow-lg">
             <CardHeader>
-                <CardTitle className="font-headline text-[25px] font-extrabold">Invigilators' Details</CardTitle>
+                <CardTitle className="font-headline text-[25px] font-extrabold text-primary">Invigilators' Details</CardTitle>
                 <CardDescription>Add all available invigilators.</CardDescription>
             </CardHeader>
             <CardContent>
