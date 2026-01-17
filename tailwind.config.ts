4c42ff20fd9b2449f7b0eaa3c8818609d8c30c5d
Lokesh D <lokeshconnects@gmail.com>
2026-01-17 18:13:04 +0000

    replace  "Montserrat"  with  "Open Sans" for headings/bold text and repl

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Roboto', 'sans-serif'],
-        headline: ['Montserrat', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Open Sans', 'sans-serif'],
         code: ['Source Code Pro', 'monospace'],
       },
       colors: {
